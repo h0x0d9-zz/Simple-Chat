1c83b379a2f89a85e638e6bf985f242b5898716f
Pavel Cherepanov <pavel@cherepanov.pro>
2018-12-04 00:39:43 +0300

    fix (app.jsx) fix import for only one selector

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import cn from 'classnames';
-import { channelsSelector } from '../selectors';
+import channelsSelector from '../selectors';
 import * as actionCreators from '../actions';
 
 const mapStateToProps = (state) => {
