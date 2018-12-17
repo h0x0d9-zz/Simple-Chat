b1f2f983ca8b84ec970bc4eddae0808354fa724c
Pavel Cherepanov <pavel@cherepanov.pro>
2018-12-18 00:28:44 +0300

    feature (ChannelsList) add control buttons fore remove and rename channels

--- src/components/ChannelsList.jsx
+++ src/components/ChannelsList.jsx
@@ -24,27 +24,56 @@ class ChannelsList extends React.Component {
     const { channels, currentChannelId } = this.props;
 
     return (
-      <ul className="nav flex-column nav-pills">
+      <ul className="nav flex-column nav-pills nav-fill">
         {
           channels.map((ch) => {
             const { id, name } = ch;
 
-            const className = cn({
+            const linkClassName = cn({
               'nav-link': true,
               active: id === currentChannelId,
             });
 
             return (
               <li className="nav-item" key={id}>
-                <a href={name} className={className} onClick={this.handleChannelChoice(id)}>
-                  #
-                  {' '}
-                  {name}
-                </a>
+                <div className="row">
+                  <div className="col-8">
+                    <a href={name} className={linkClassName} onClick={this.handleChannelChoice(id)}>
+                      #&nbsp;
+                      {name}
+                    </a>
+                  </div>
+                  <div className="col-4 mt-1">
+                    <div className="btn-group" role="group" aria-label="Channel Control">
+                      <button type="button" className="btn-link btn btn-sm">
+                        <i className="far fa-edit" />
+                      </button>
+                      <button type="button" disabled className="btn-link btn btn-sm">
+                        <i className="far fa-trash-alt" />
+                      </button>
+                    </div>
+                  </div>
+                </div>
+
               </li>
             );
           })
         }
+        <li className="nav-item">
+          <div className="row">
+            <div className="col-9">
+              <a href="#editChannel" className="nav-link">
+                Add channel
+              </a>
+            </div>
+            <div className="col-3  mt-1">
+              <button type="button" className="btn-link btn btn-sm">
+                <i className="fas fa-plus" />
+              </button>
+            </div>
+          </div>
+
+        </li>
       </ul>
     );
   }
